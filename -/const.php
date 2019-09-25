3bdbed3c1441bfe4ec9e66f0928e9fdf2230d791
Frank Bueltge <frank@bueltge.de>
2019-09-25 10:31:34 +0200

    Add new version string

--- -/const.php
+++ -/const.php
@@ -3,22 +3,25 @@
  * Define all constants.
  *
  * @since   2015-12-01
+ * @version 2019-09-25
  * @package Lessn
  */
 
-define('LESSN_VERSION', '1.2.0');
+define('LESSN_VERSION', '1.2.1');
 
 define('LESSN_ROOT', __DIR__); // added in 1.2.0
 
-define('LESSN_DOMAIN',  preg_replace('#^www\.#', '', $_SERVER['SERVER_NAME']));
-define('LESSN_URL',     str_replace('-/index.php', '',  $_SERVER['REQUEST_SCHEME'] . '://' . LESSN_DOMAIN.$_SERVER['PHP_SELF']));
+define('LESSN_DOMAIN', preg_replace('#^www\.#', '', $_SERVER['SERVER_NAME']));
+define('LESSN_URL', str_replace('-/index.php', '', $_SERVER['REQUEST_SCHEME'].'://'.LESSN_DOMAIN.$_SERVER['PHP_SELF']));
 
-define('COOKIE_NAME',   DB_PREFIX.'auth');
-define('COOKIE_VALUE',  md5(USERNAME.PASSWORD.COOKIE_SALT));
+define('COOKIE_NAME', DB_PREFIX.'auth');
+define('COOKIE_VALUE', md5(USERNAME.PASSWORD.COOKIE_SALT));
 define('COOKIE_DOMAIN', '.'.LESSN_DOMAIN);
 
-if (!defined('API_SALT')) define('API_SALT', 'L35sm4K35M0U7hSAP1'); // added in 1.0.5
+if (!defined('API_SALT')) {
+    define('API_SALT', 'L35sm4K35M0U7hSAP1');
+} // added in 1.0.5
 define('API_KEY', md5(USERNAME.PASSWORD.API_SALT));
 
-define('NOW',           time());
-define('YEAR',          365 * 24 * 60 * 60);
+define('NOW', (new DateTime())->getTimestamp());
+define('YEAR', 365 * 24 * 60 * 60);
