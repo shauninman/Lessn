e7b561a9db23000e82f00823f2d24fb49938b44c
Frank Bueltge <frank@bueltge.de>
2019-09-25 12:45:11 +0200

    More solid path

--- -/pages/add.php
+++ -/pages/add.php
@@ -1,23 +1,23 @@
-<?php include('stubs/header.php'); ?>
-<form method="get">
-	<input type="text" id="url" name="url" placeholder="url" />
-	<button>Lessn</button>
+<?php require_once(__DIR__.'../header.php'); ?>
+    <form method="get">
+        <label for="url"><input type="text" id="url" name="url" placeholder="url" /></label>
+        <button>Lessn</button>
 
-	<p>Grab the <a
-		title="Lessn a link"
-		href="javascript:location.href='<?php echo LESSN_URL; ?>-/?url='+encodeURIComponent(location.href);"
-		onclick="alert('Drag this bookmarklet onto your browser bar.');return false;">
-		Lessn
-		</a> or <a
-		title="Lessn and tweet the Lessn'd link"
-		href="javascript:location.href='<?php echo LESSN_URL; ?>-/?tweet&amp;url='+encodeURIComponent(location.href);"
-		onclick="alert('Drag this bookmarklet onto your browser bar.');return false;">
-		Tweetn
-		</a> bookmarklet.
-		<span>API key: <code><?php echo API_KEY; ?></code></span>
-	</p>
-</form>
-<script>
-document.getElementById('url').focus();
-</script>
-<?php include('stubs/footer.php');
+        <p>Grab the <a
+                title="Lessn a link"
+                href="javascript:location.href='<?php echo LESSN_URL; ?>-/?url='+encodeURIComponent(location.href);"
+                onclick="alert('Drag this bookmarklet onto your browser bar.');return false;">
+                Lessn
+            </a> or <a
+                title="Lessn and tweet the Lessn'd link"
+                href="javascript:location.href='<?php echo LESSN_URL; ?>-/?tweet&amp;url='+encodeURIComponent(location.href);"
+                onclick="alert('Drag this bookmarklet onto your browser bar.');return false;">
+                Tweetn
+            </a> bookmarklet.
+            <span>API key: <code><?php echo API_KEY; ?></code></span>
+        </p>
+    </form>
+    <script>
+        document.getElementById('url').focus();
+    </script>
+<?php require_once(__DIR__.'../stubs/footer.php');
