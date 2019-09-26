a74120d60d6043144612732c51841730b2b5f4b8
bueltge <frank@bueltge.de>
2019-09-26 17:35:31 +0200

    update style

--- -/db.php
+++ -/db.php
@@ -9,10 +9,12 @@
 error_reporting(0);
 
 $db = SIDB(DB_NAME, DB_USERNAME, DB_PASSWORD, DB_SERVER);
-$db->query('CREATE TABLE IF NOT EXISTS `'.DB_PREFIX.'urls` ( '.
-	'`id` int(11) unsigned NOT NULL auto_increment, '.
-	'`url` text character set utf8 collate utf8_unicode_ci NOT NULL, '.
-	'`checksum` int(10) unsigned NOT NULL, '.
-	'PRIMARY KEY  (`id`), '.
-	'KEY `checksum` (`checksum`) '.
-	') ENGINE=MyISAM DEFAULT CHARSET=utf8;');
+$db->query(
+    'CREATE TABLE IF NOT EXISTS `'.DB_PREFIX.'urls` ( '.
+    '`id` int(11) unsigned NOT NULL auto_increment, '.
+    '`url` text character set utf8 collate utf8_unicode_ci NOT NULL, '.
+    '`checksum` int(10) unsigned NOT NULL, '.
+    'PRIMARY KEY  (`id`), '.
+    'KEY `checksum` (`checksum`) '.
+    ') ENGINE=MyISAM DEFAULT CHARSET=utf8;'
+);
