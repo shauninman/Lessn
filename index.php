c3291092f29838ab789cc9375a54edfbf71010f3
bueltge <frank@bueltge.de>
2019-09-26 12:20:08 +0200

    Use PSR2 as codestyle format

--- index.php
+++ index.php
@@ -6,18 +6,17 @@ include('-/db.php');
 require_once '-/const.php';
 
 // redirect
-if (isset($_GET['token']))
-{
-	@list($token, $ext) = explode('.', $_GET['token'], 2);
-	if ($db->query('SELECT * FROM `'.DB_PREFIX.'urls` WHERE id='.base_convert($token, 36, 10).' LIMIT 1')) {
-		if ($rows = $db->rows()) {
-			$row = $rows[0];
+if (isset($_GET['token'])) {
+    @list($token, $ext) = explode('.', $_GET['token'], 2);
+    if ($db->query('SELECT * FROM `'.DB_PREFIX.'urls` WHERE id='.base_convert($token, 36, 10).' LIMIT 1')) {
+        if ($rows = $db->rows()) {
+            $row = $rows[0];
 
-			header($_SERVER['SERVER_PROTOCOL'].' 301 Moved Permanently');
-			header('Location:'.stripslashes($row['url']));
-			exit();
-		}
-	}
+            header($_SERVER['SERVER_PROTOCOL'].' 301 Moved Permanently');
+            header('Location:'.stripslashes($row['url']));
+            exit();
+        }
+    }
 }
 
 // no redirect
